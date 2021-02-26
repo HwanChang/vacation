c69cad2eaeebb051e79c5dd86a25b76e4f3c91d4
HwanChang <hwanchang.dev@gmail.com>
2021-02-26 16:19:38 +0900

    init: 프로젝트 초기 구성

---
+++ src/test/java/com/hwanchang/vacation/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwanchang.vacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
