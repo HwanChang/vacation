9c6f4b276b10787f7c232a60063f6dca7779bf42
HwanChang <hwanchang.dev@gmail.com>
2021-03-10 14:59:36 +0900

    test: 내 휴가 신청 내용 조회 테스트 코드 추가

--- src/test/java/com/hwanchang/vacation/service/application/ApplicationServiceTest.java
+++ src/test/java/com/hwanchang/vacation/service/application/ApplicationServiceTest.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -38,6 +39,10 @@ class ApplicationServiceTest {
 
     List<ApproveRequest> approveRequests = new ArrayList<>();
 
+    User test01 = new User("test01@gmail.com", "test01", "P@ssword1", "010-1111-1111");
+    User test02 = new User("test02@gmail.com", "test02", "P@ssword2", "010-2222-2222");
+    User test03 = new User("test03@gmail.com", "test03", "P@ssword3", "010-3333-3333");
+
     @BeforeAll
     void setUp() {
         vacationDates = List.of(LocalDate.of(2021, 3, 9), LocalDate.of(2021, 3, 10));
@@ -57,9 +62,9 @@ class ApplicationServiceTest {
     @Order(1)
     void 휴가_신청_등록() {
         //given
-        User requestUser = userService.join("test01@gmail.com", "test01", "P@ssword1", "010-1111-1111");
-        User firstApprovalUser = userService.join("test02@gmail.com", "test02", "P@ssword2", "010-2222-2222");
-        User secondApprovalUser = userService.join("test03@gmail.com", "test03", "P@ssword3", "010-3333-3333");
+        User requestUser = userService.join(test01.getEmail(), test01.getName(), test01.getPassword(), test01.getPhone());
+        User firstApprovalUser = userService.join(test02.getEmail(), test02.getName(), test02.getPassword(), test02.getPhone());
+        User secondApprovalUser = userService.join(test03.getEmail(), test03.getName(), test03.getPassword(), test03.getPhone());
 
         log.info("RequestUser: {}", requestUser);
         log.info("FirstApprovalUser: {}", firstApprovalUser);
@@ -79,6 +84,37 @@ class ApplicationServiceTest {
         assertThat(application.getApproves().stream().map(Approve::getUser)).contains(secondApprovalUser);
 
         log.info("Application: {}", application);
+    }
+
+    @Test
+    @Transactional
+    @Order(2)
+    void 내_휴가_전체조회() {
+        //given
+        User requestUser = userService.findById(1L).orElse(null);
+        User firstApprovalUser = userService.findById(2L).orElse(null);
+        User secondApprovalUser = userService.findById(3L).orElse(null);
+
+        log.info("RequestUser: {}", requestUser);
+        log.info("FirstApprovalUser: {}", firstApprovalUser);
+        log.info("SecondApprovalUser: {}", secondApprovalUser);
+
+        //when
+        List<Application> applications = applicationService.findAll(1L);
+
+        //then
+        assertThat(applications).hasSizeGreaterThan(0);
+        for (Application application : applications) {
+            assertThat(application.getLevel()).isEqualTo(1);
+            assertThat(application.getUser()).isEqualTo(requestUser);
+            assertThat(application.getVacations().stream().map(Vacation::getDate)).containsAll(vacationDates);
+            assertThat(application.getVacations().stream().map(Vacation::getReason)).contains(reason);
+            assertThat(application.getApproves()).hasSize(2);
+            assertThat(application.getApproves().stream().map(Approve::getUser)).contains(firstApprovalUser);
+            assertThat(application.getApproves().stream().map(Approve::getUser)).contains(secondApprovalUser);
+
+            log.info("Application: {}", application);
+        }
     }
 
 }
